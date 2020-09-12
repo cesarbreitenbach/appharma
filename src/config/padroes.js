c3d4a1e7242a41c6140ee714378c0705a8ffc1d6
breitenbach <cesar.breitenbach@gmail.com>
2020-09-12 10:45:14 -0300

    Alteracoes de configuracao

--- src/config/padroes.js
+++ src/config/padroes.js
@@ -2,6 +2,7 @@ export default {
    corPrincipal:'#1b5755',
    corSecundaria:'#34ada9',
    corModal:'#2a918e',
+   API_URL:"https://approachmobile.company/api",
    URL_FILES:"https://approachmobile.company/files/",
    corSelecionado:'#326ded'
 }
--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
+import d from '../config/padroes'
 
 const api = axios.create({
-  baseURL:'https://approachmobile.company/api'
+  baseURL:d.API_URL
 })
 
 export default api;
